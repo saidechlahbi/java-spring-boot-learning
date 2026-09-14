ec2141edddd31595caf89896c7a7a7bce787d4ed
Said Echlahbi <sechlahb@c2r7p3.1337.ma>
2026-09-14 15:39:37 +0100

    update

--- Java/day01/ex00/Transaction.java
+++ Java/day01/ex00/Transaction.java
@@ -1,10 +1,21 @@
 import java.util.UUID;
+public class User;
 public class Transaction
 {
-    private:
-        UUID Identifier;
-        Recipient (User type)
-        Sender (User type)
-        Transfer category (debits, credits)
-        Transfer amount
+    public enum category
+    { 
+        DEBIT, CREDIT 
+    }
+    private UUID Identifier;
+    private User Recipient;
+    private User Sender;
+    private category TransferCategory;
+
+    private double Amount;
+
+    public Transaction(UUID id, User recepient, User sender, category TransferCategory, double amount)
+    {
+        
+    }
+
 }
--- Java/day01/ex00/User.java
+++ Java/day01/ex00/User.java
@@ -1,7 +1,41 @@
 public class User
 {
-    private:
-        int Identifier;
-        String Name;
-        long Balance;
+    private int Identifier;
+    private String Name;
+    private long Balance;
+
+    public User(int id, String name, long balance)
+    {
+        if (balance < 0)
+        {
+            throw new IllegalArgumentException("Initial balance cannot be negative");
+        }
+    }
+    ~User();
+    public void setID(int id)
+    {
+        Identifier = id;
+    }
+    public void setNAME(String name)
+    {
+        Name = name;
+    }   
+    public void set(long balance)
+    {
+        Balance = balance;
+    }
+
+    public int getID()
+    {
+        return identifier;
+    }
+    public String getNAME()
+    {
+        return Name;
+    }
+    public long getBalance()
+    {
+        return Balance;
+    }
+
 }
